644c209ab8d7946050d87b073a24b8c671bfbf96
Pierre Kisters <pierrekisters@gmail.com>
2020-07-27 00:36:31 +0200

    blocks are shifting!

--- src/main/scala/de/lolhens/blockshifter/BlockshifterMod.scala
+++ src/main/scala/de/lolhens/blockshifter/BlockshifterMod.scala
@@ -4,7 +4,6 @@ import de.lolhens.blockshifter.block.RailBlock
 import net.fabricmc.api.ModInitializer
 import net.fabricmc.loader.api.FabricLoader
 import net.fabricmc.loader.api.metadata.ModMetadata
-import net.minecraft.block.entity.BlockEntityType
 import net.minecraft.item.{BlockItem, Item, ItemGroup}
 import net.minecraft.util.Identifier
 import net.minecraft.util.registry.Registry
@@ -19,14 +18,9 @@ object BlockshifterMod extends ModInitializer {
 
   val railBlockId = new Identifier(metadata.getId, "rail")
   val railBlock: RailBlock = new RailBlock()
-  val railBlockEntity: BlockEntityType[_] = railBlock.blockEntityType
 
   override def onInitialize(): Unit = {
     Registry.register(Registry.BLOCK, railBlockId, railBlock)
     Registry.register(Registry.ITEM, railBlockId, new BlockItem(railBlock, new Item.Settings().group(ItemGroup.BUILDING_BLOCKS)))
-    Registry.register(Registry.BLOCK_ENTITY_TYPE, railBlockId, railBlockEntity)
-
-    //ServerTickEvents.START_WORLD_TICK.register()
-    //ServerTickEvents.END_WORLD_TICK.register()
   }
 }
--- src/main/scala/de/lolhens/blockshifter/block/RailBlock.scala
+++ src/main/scala/de/lolhens/blockshifter/block/RailBlock.scala
@@ -2,18 +2,79 @@ package de.lolhens.blockshifter.block
 
 import net.fabricmc.fabric.api.`object`.builder.v1.block.FabricBlockSettings
 import net.minecraft.block._
-import net.minecraft.block.entity.{BlockEntity, BlockEntityType}
-import net.minecraft.block.piston.PistonBehavior
-import net.minecraft.world.BlockView
+import net.minecraft.entity.player.PlayerEntity
+import net.minecraft.util.hit.BlockHitResult
+import net.minecraft.util.math.{BlockPos, Direction}
+import net.minecraft.util.{ActionResult, Hand}
+import net.minecraft.world.World
 
-class RailBlock() extends Block(RailBlock.settings) with BlockEntityProvider {
-  val blockEntityType: BlockEntityType[RailBlockEntity] =
-    BlockEntityType.Builder.create(() => new RailBlockEntity(blockEntityType), this)
-      .build(null)
+class RailBlock() extends Block(RailBlock.settings) {
+  private def shiftBlocks(world: World, pos: BlockPos, facing: Direction, moveDirection: Direction): Unit = {
+    def follow(start: BlockPos, direction: Direction): Iterator[BlockPos] =
+      Iterator.iterate(start)(_.offset(direction))
 
-  override def createBlockEntity(world: BlockView): BlockEntity = blockEntityType.instantiate()
+    def isRail(pos: BlockPos): Boolean = world.getBlockState(pos).isOf(this)
 
-  override def getPistonBehavior(state: BlockState): PistonBehavior = PistonBehavior.BLOCK
+    def isAir(pos: BlockPos): Boolean = world.getBlockState(pos).isAir
+
+    val railStartPos: BlockPos = follow(pos, moveDirection.getOpposite).takeWhile(isRail).toList.last
+    val thisRailLength: Int = follow(railStartPos, moveDirection).takeWhile(isRail).size
+    println("first length: " + thisRailLength)
+    follow(railStartPos, facing)
+      .take(RailBlock.maxRailDistance)
+      .zipWithIndex
+      .drop(1)
+      .find(e => isRail(e._1))
+      .filter(_._2 > 0)
+      .foreach {
+        case (otherRailPos, railDistance) =>
+          println("distance: " + railDistance)
+          val otherRailLength: Int = follow(otherRailPos, moveDirection).takeWhile(isRail).size
+          val railLength: Int = Math.min(thisRailLength, otherRailLength)
+
+          def betweenRails(posOnRail: BlockPos): Iterator[BlockPos] =
+            follow(posOnRail.offset(facing), facing).take(railDistance - 1)
+
+          val emptyRows: Int = {
+            val railEndPos: BlockPos = railStartPos.offset(moveDirection, railLength - 1)
+            follow(railEndPos, moveDirection.getOpposite)
+              .take(railLength)
+              .takeWhile(betweenRails(_).forall(isAir))
+              .size
+          }
+
+          println("empty rows: " + emptyRows)
+
+          val overhang: Int = Math.max(0, {
+            follow(railStartPos, moveDirection.getOpposite)
+              .take(emptyRows + 1)
+              .takeWhile(!betweenRails(_).forall(isAir))
+              .size
+          } - 1)
+
+          println("overhang: " + overhang)
+
+          val rowsToMove = railLength - 1 - Math.max(0, emptyRows - 1)
+          val rowsToMoveOffset = Math.max(0, rowsToMove - 1)
+
+          val firstRowToMove: BlockPos = railStartPos.offset(moveDirection, rowsToMoveOffset)
+
+          if (betweenRails(firstRowToMove.offset(moveDirection)).forall(isAir)) {
+            follow(firstRowToMove, moveDirection.getOpposite)
+              .take(rowsToMove + overhang)
+              .foreach(betweenRails(_).foreach { pos =>
+                world.setBlockState(pos.offset(moveDirection), world.getBlockState(pos))
+                world.removeBlock(pos, false)
+              })
+          }
+      }
+
+  }
+
+  override def onUse(state: BlockState, world: World, pos: BlockPos, player: PlayerEntity, hand: Hand, hit: BlockHitResult): ActionResult = {
+    shiftBlocks(world, pos, Direction.NORTH, Direction.UP)
+    ActionResult.SUCCESS
+  }
 }
 
 object RailBlock {
@@ -22,5 +83,7 @@ object RailBlock {
       .of(Material.STONE)
       .requiresTool()
       .hardness(2.0F)
+
+  val maxRailDistance: Int = 64
 }
 
