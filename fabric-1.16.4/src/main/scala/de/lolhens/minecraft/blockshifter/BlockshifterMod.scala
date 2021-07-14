bdf9e00d67bdbe7b0b316cccc9be1447bec64c56
Pierre Kisters <pierrekisters@gmail.com>
2021-07-14 19:47:51 +0200

    updates

--- fabric-1.16.4/src/main/scala/de/lolhens/minecraft/blockshifter/BlockshifterMod.scala
+++ fabric-1.16.4/src/main/scala/de/lolhens/minecraft/blockshifter/BlockshifterMod.scala
@@ -4,21 +4,14 @@ import de.lolhens.minecraft.blockshifter.block.RailBlock
 import de.lolhens.minecraft.blockshifter.util.EntityMover
 import net.fabricmc.api.ModInitializer
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents
-import net.fabricmc.loader.api.FabricLoader
-import net.fabricmc.loader.api.metadata.ModMetadata
 import net.minecraft.item.{BlockItem, Item, ItemGroup}
 import net.minecraft.util.Identifier
 import net.minecraft.util.registry.Registry
 
-import scala.jdk.CollectionConverters._
-
 object BlockshifterMod extends ModInitializer {
-  val metadata: ModMetadata = {
-    FabricLoader.getInstance().getEntrypointContainers("main", classOf[ModInitializer])
-      .iterator().asScala.find(this eq _.getEntrypoint).get.getProvider.getMetadata
-  }
+  val id: String = "blockshifter"
 
-  val RAIL_BLOCK_ID = new Identifier(metadata.getId, "rail")
+  val RAIL_BLOCK_ID = new Identifier(id, "rail")
   val RAIL_BLOCK: RailBlock = new RailBlock()
 
   override def onInitialize(): Unit = {
